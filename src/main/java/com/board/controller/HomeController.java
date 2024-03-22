e39243143dee3dfcb5788422778099187adcc862
pmj8298 <pmj8298@naver.com>
2024-03-22 12:22:24 +0900

    BoardMenu Copy

---
+++ src/main/java/com/board/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	// http://localhost:9090
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+}
