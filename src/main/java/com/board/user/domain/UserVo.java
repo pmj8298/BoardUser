307c70fd410df71f1d83b365603af8febaba42f6
pmj8298 <pmj8298@naver.com>
2024-03-22 16:50:05 +0900

    수정

--- src/main/java/com/board/user/domain/UserVo.java
+++ src/main/java/com/board/user/domain/UserVo.java
@@ -2,15 +2,17 @@ package com.board.user.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-//@Data: @Getter @Setter @ToString @NoArgsConstructor @EqualsAndHashCode @RequiredArgConstructor
+//@Data: @Getter @Setter @ToString @EqualsAndHashCode @RequiredArgConstructor
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserVo {
 	private String userid;
 	private String passwd;
 	private String username;
 	private String email;
-	private int    upoint;
+	private String upoint;
 	private String indate;
 }
